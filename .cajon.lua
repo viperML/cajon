5931dc9e1c4930ecd2ed0ff1b12d18022ff29a96
Fernando Ayats <ayatsfer@gmail.com>
2026-03-17 15:12:27 +0100

    Add subcommands

--- .cajon.lua
+++ .cajon.lua
@@ -1,6 +1,7 @@
 return {
 	image = "debian",
 	cook_script = [[
+    apt update -y
     apt install -y vim
   ]],
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use std::collections::BTreeMap;
 use std::collections::HashMap;
 use std::fs;
+use std::hash::DefaultHasher;
+use std::hash::Hash;
+use std::hash::Hasher;
+use std::os::unix::process::CommandExt;
 use std::path::PathBuf;
 use std::process::Command;
+use std::process::Stdio;
 use std::sync::OnceLock;
 
 use clap::Parser;
 use color_eyre::Result;
 use color_eyre::eyre::Context;
+use color_eyre::eyre::bail;
 use mlua::LuaSerdeExt;
 use mlua::prelude::*;
 use serde::Deserialize;
@@ -36,13 +43,13 @@ fn default_name() -> String {
     format!("cajon-{dirname}")
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Hash)]
 struct Config {
     image: String,
     #[serde(default = "default_true")]
     mount_cwd: bool,
     #[serde(default)]
-    env: HashMap<String, String>,
+    env: BTreeMap<String, String>,
     #[serde(default)]
     volumes: Vec<String>,
     script: Option<String>,
@@ -54,6 +61,15 @@ struct Config {
     workdir: String,
     #[serde(default = "default_name")]
     name: String,
+}
+
+impl Config {
+    fn hash_str(&self) -> String {
+        let mut hasher = DefaultHasher::new();
+        self.hash(&mut hasher);
+        let hash = hasher.finish();
+        format!("{hash:016x}")
+    }
 }
 
 impl Cli {
@@ -88,6 +104,7 @@ struct InspectState {
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 struct InspectConfig {
+    cmd: String,
     annotations: HashMap<String, String>,
 }
 
@@ -98,31 +115,117 @@ struct InspectOutput {
     config: InspectConfig,
 }
 
-fn inspect_container(config: &Config) -> Result<Option<InspectOutput>> {
-    let mut cmd = Command::new("podman");
-    cmd.args(&[
-        "container",
-        "inspect",
-        config.name.as_str(),
-        "--format",
-        "json",
-    ]);
+impl Config {
+    fn inspect_container(&self) -> Result<Option<InspectOutput>> {
+        let mut cmd = Command::new("podman");
+        cmd.args(&[
+            "container",
+            "inspect",
+            self.name.as_str(),
+            "--format",
+            "json",
+        ]);
 
-    print_command(&cmd);
+        print_command(&cmd);
 
-    let output = cmd.output().wrap_err("inspecting container")?;
-    let stdout = String::from_utf8(output.stdout)?;
+        let output = cmd.output().wrap_err("inspecting container")?;
+        let stdout = String::from_utf8(output.stdout)?;
 
-    if output.status.success() == false {
-        return Ok(None);
+        if output.status.success() == false {
+            return Ok(None);
+        }
+
+        let mut res: Vec<InspectOutput> =
+            serde_json::from_str(&stdout).wrap_err("deserializing output")?;
+
+        let res2 = res.pop();
+
+        return Ok(res2);
     }
 
-    let mut res: Vec<InspectOutput> =
-        serde_json::from_str(&stdout).wrap_err("deserializing output")?;
+    fn run(&self) -> Result<()> {
+        let mut cmd = Command::new("podman");
 
-    let res2 = res.pop();
+        cmd.args(&["run", "--interactive", "--tty"]);
 
-    return Ok(res2);
+        if !self.stateful {
+            cmd.arg("--rm");
+        }
+
+        cmd.arg("--workdir");
+        cmd.arg(&self.workdir);
+
+        let hash = self.hash_str();
+        cmd.arg("--annotation");
+        cmd.arg(format!("cajon.hash={hash}"));
+
+        cmd.arg("--name");
+        cmd.arg(&self.name);
+
+        cmd.arg(&self.image);
+
+        print_command(&cmd);
+
+        bail!(cmd.exec());
+    }
+
+    fn attach(&self) -> Result<()> {
+        let mut cmd = Command::new("podman");
+        cmd.args(&["exec", "--interactive", "--tty"]);
+        cmd.arg(&self.name);
+        cmd.arg("/bin/sh");
+        print_command(&cmd);
+        bail!(cmd.exec());
+    }
+
+    fn start(&self) -> Result<()> {
+        let mut cmd = Command::new("podman");
+        cmd.args(&["start", "--attach", "--interactive"]);
+        cmd.arg(&self.name);
+        print_command(&cmd);
+        bail!(cmd.exec());
+    }
+
+    fn destroy(&self) -> Result<()> {
+        let mut cmd = Command::new("podman");
+        cmd.args(&["container", "rm", "--force", "--volumes", "--ignore"]);
+        cmd.arg(&self.name);
+
+        cmd.stderr(Stdio::null());
+        cmd.stdout(Stdio::null());
+
+        print_command(&cmd);
+        cmd.status()?;
+
+        Ok(())
+    }
+
+    fn cook(&self) -> Result<()> {
+        let cook_script = self
+            .cook_script
+            .as_deref()
+            .expect("cook called without cook_script");
+
+        let mut cmd = Command::new("podman");
+        cmd.args(&["run", "--name"]);
+        cmd.arg(&self.name);
+
+        let hash = self.hash_str();
+        cmd.arg("--annotation");
+        cmd.arg(format!("cajon.hash={hash}"));
+
+        cmd.arg("--workdir");
+        cmd.arg(&self.workdir);
+
+        cmd.arg(&self.image);
+        cmd.args(&["/bin/sh", "-c"]);
+        cmd.arg(cook_script);
+
+        print_command(&cmd);
+        cmd.status().wrap_err("running cook script")?;
+
+        self.start()
+    }
 }
 
 fn main() -> Result<()> {
@@ -130,7 +233,7 @@ fn main() -> Result<()> {
 
     let cli = Cli::global();
 
-    let config: Config = {
+    let mut config: Config = {
         let contents =
             fs::read_to_string(&cli.config_file).wrap_err("reading configuration file")?;
         let lua = Lua::new();
@@ -138,10 +241,41 @@ fn main() -> Result<()> {
             .wrap_err("parsing configuration")?
     };
 
-    println!("{config:#?}");
+    config.stateful = config.stateful || config.cook_script.is_some();
+    if let Some(s) = config.cook_script.as_mut() {
+        *s = textwrap::dedent(s.as_str()).trim().to_string();
+    }
 
-    let inspect = inspect_container(&config)?;
-    println!("{inspect:#?}");
+    if !config.stateful {
+        config.destroy()?;
+        return config.run();
+    }
 
-    Ok(())
+    let inspect = config.inspect_container()?;
+    let new_hash = config.hash_str();
+
+    let old_hash = inspect
+        .as_ref()
+        .and_then(|o| o.config.annotations.get("cajon.hash"))
+        .map(String::as_str)
+        .unwrap_or("");
+
+    let container_valid = inspect.is_some() && old_hash == new_hash;
+
+    if config.cook_script.is_some() && !container_valid {
+        config.destroy()?;
+        return config.cook();
+    }
+
+    if !container_valid {
+        config.destroy()?;
+        return config.run();
+    }
+
+    let o = inspect.as_ref().unwrap();
+    if o.state.running {
+        config.attach()
+    } else {
+        config.start()
+    }
 }
